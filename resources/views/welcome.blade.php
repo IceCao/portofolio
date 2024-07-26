1eaeaca12e94e44141a9fd38ac535a6cbf302ae0
Riski <keluargapurnama123@gmail.com>
2024-07-26 15:40:13 +0700

    Laravel 7

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 7 with Bootstrap 5</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</nav>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</body>
